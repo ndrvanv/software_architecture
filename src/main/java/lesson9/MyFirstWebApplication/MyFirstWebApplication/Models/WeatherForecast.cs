e866176f6d56d06b1f3ed68a95edd90e6c106320
Andrew <it.andr@outlook.com>
2023-11-09 18:13:14 +0300

    lesson 9

---
+++ src/main/java/lesson9/MyFirstWebApplication/MyFirstWebApplication/Models/WeatherForecast.cs
@@ -0,0 +1,26 @@
+﻿namespace MyFirstWebApplication.Models
+{
+    /// <summary>
+    /// Прогноз погоды
+    /// </summary>
+    public class WeatherForecast
+    {
+        /// <summary>
+        /// Дата измерения
+        /// </summary>
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// Температура градус цельсия
+        /// </summary>
+        public int TemperatureC { get; set; }
+
+        /// <summary>
+        /// Температура градус франгейта
+        /// </summary>
+        public int TemperatureF
+        {
+            get { return 32 + (int)(TemperatureC / 0.5556); }
+        }
+    }
+}
