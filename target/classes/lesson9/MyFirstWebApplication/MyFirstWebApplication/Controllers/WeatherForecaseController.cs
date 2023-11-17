a53afce61709bced703f64169119862593b855ff
Andrew <it.andr@outlook.com>
2023-11-17 13:46:57 +0300

    lesson 12

---
+++ target/classes/lesson9/MyFirstWebApplication/MyFirstWebApplication/Controllers/WeatherForecaseController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyFirstWebApplication.Models;
+
+namespace MyFirstWebApplication.Controllers
+{
+    
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeatherForecaseController : ControllerBase
+    {
+
+        private WeatherForecaseController _weatherForecaseController;
+
+        public WeatherForecaseController(WeatherForecaseController weatherForecaseController)
+        {
+            _weatherForecaseController = weatherForecaseController;
+        }
+
+        [HttpPost("add-data")]
+        public ActionResult<bool> Add([FromQuery]DateTime date, [FromQuery] int temperatureC)
+        {
+            bool result = _weatherForecaseController.Add(date, temperatureC);
+            return Ok(result);
+        }
+
+        [HttpPut("update-data")]
+        public ActionResult<bool> Update([FromQuery] DateTime date, [FromQuery] int temperatureC)
+        {
+            bool result = _weatherForecaseController.Update(date, temperatureC);
+            return Ok(result);
+        }
+        [HttpDelete("delete-data")]
+        public ActionResult<List<WeatherForecast>> Delete([FromQuery] DateTime date)
+        {
+            List<WeatherForecast> weatherForecasts = _weatherForecaseController.Get(date, date);
+            foreach (WeatherForecast weatherForecast in weatherForecasts)
+            {
+                _weatherForecaseController.Delete(weatherForecast);
+            }
+            return Ok();
+        }
+
+        [HttpGet("get-data")]
+        public ActionResult<List<WeatherForecast>> Get([FromQuery] DateTime from, [FromQuery] DateTime to)
+        {
+            List<WeatherForecast> result = _weatherForecaseController.Get(from, to);
+            return Ok(result);
+        }
+    }
+}
---
+++ target/classes/lesson9/MyFirstWebApplication/MyFirstWebApplication/Models/WeatherForecastHolder.cs
@@ -0,0 +1,101 @@
+﻿using System.Data;
+
+namespace MyFirstWebApplication.Models
+{
+    /// <summary>
+    /// Объект на базе класса WeatherForecastHolder будет хранить список показаний по температуре
+    /// </summary>
+    public class WeatherForecastHolder
+    {
+        //private static WeatherForecastHolder _instance;
+        //public static WeatherForecastHolder Instance()
+        //{
+        //    if (_instance == null)
+        //        _instance = new WeatherForecastHolder();
+        //    return _instance;
+        //}
+
+
+
+        // Коллекция для хранения по температуре
+        private List<WeatherForecast> _values;
+
+       public WeatherForecastHolder()
+        {
+            _values = new List<WeatherForecast>();
+        }
+
+        /// <summary>
+        /// Добавить новый показатель по температуре
+        /// </summary>
+        /// <param name="date"></param>
+        /// <param name="temperature"></param>
+        public bool Add(DateTime date, int temperatureС)
+        {
+            WeatherForecast forecast = new WeatherForecast();
+            forecast.TemperatureC = temperatureС;
+            forecast.Date = date;
+            _values.Add(forecast);
+            return true;
+        }
+
+        /// <summary>
+        /// Обновить показатели по температуре
+        /// </summary>
+        /// <param name="date"></param>
+        /// <param name="temperatureС"></param>
+        /// <returns></returns>
+        public bool Update(DateTime date, int temperatureС)
+        {
+            foreach (WeatherForecast forecast in _values)
+            {
+                if (forecast.Date == date)
+                {
+                    forecast.TemperatureC = temperatureС;
+                    return true;
+                }
+            }
+            return false;
+
+        }
+
+        /// <summary>
+        /// Удалить показания по температуре за указанную дату
+        /// </summary>
+        /// <param name="date"></param>
+        /// <returns></returns>
+        public bool Delete(DateTime date)
+        {
+            // Находим все показания по температуре для указанной даты
+            List<WeatherForecast> forecasts = Get(date, date);
+
+            // Удаляем все найденные показания
+            foreach (WeatherForecast forecast in forecasts)
+            {
+                _values.Remove(forecast);
+            }
+
+            return forecasts.Count > 0;
+        }
+
+        /// <summary>
+        /// Получить показатели температуры за период
+        /// </summary>
+        /// <param name="from"></param>
+        /// <param name="to"></param>
+        /// <returns></returns>
+        public List<WeatherForecast> Get(DateTime from, DateTime to)
+        {
+            List<WeatherForecast> resultList = new List<WeatherForecast>();
+            foreach (WeatherForecast forecast in _values)
+            {
+                if (forecast.Date >= from && forecast.Date <= to)
+                {
+                    resultList.Add(forecast);
+                }
+            }
+            return resultList;
+        }
+    }
+    
+}
